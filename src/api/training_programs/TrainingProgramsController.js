2354e02d757d8df47d95d9a9b95fb482b97580b9
Rurouni Kenshin <stramvasov@gmail.com>
2025-02-19 21:36:22 +0500

    update

--- src/api/training_programs/TrainingProgramsController.js
+++ src/api/training_programs/TrainingProgramsController.js
@@ -2,6 +2,6 @@ import apiClient from '../apiClient';
 
 export default class TrainingProgramsController {
   static async getTrainingProgramsList(params) {
-    return await apiClient.get('/training_programs', params);
+    return await apiClient.get('/training_programs', { params });
   }
 }
--- src/pages/Dashboard/components/TrainingPrograms/index.jsx
+++ src/pages/Dashboard/components/TrainingPrograms/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { Box, Grid2, Skeleton, Stack, useTheme, Typography, Divider } from "@mui/material";
+import { Box, Grid2, Skeleton, Stack, useTheme, Typography, Divider, Chip, Grid } from "@mui/material";
 import { getWorkoutsList } from "@store/slices/Workouts/workouts.thunks";
 import { getMuscleGroupUsedCount } from "@store/slices/Exercises/exercises.thunks";
 import { isSuccess, isLoading, isFailed } from "@constants/redux.constants";
@@ -19,7 +19,7 @@ function TrainingPrograms() {
   console.log('t', trainingProgramsList.data)
 
   useEffect(() => {
-    dispatch(getTrainingProgramsList())
+    dispatch(getTrainingProgramsList({ is_active: true }))
   }, [])
   
 
@@ -32,14 +32,14 @@ function TrainingPrograms() {
           gap: 0,
         }}
       >
-        <Typography variant="h5" sx={{ pb: 2 }}>Программа тренировок</Typography>
+        <Typography variant="h5" sx={{ pb: 2 }}>Активные программы тренировок</Typography>
 
         {isFailed(trainingProgramsList.loadingStatus) && (<UIAlert />)}
 
         {/* loading */}
 
         {(isSuccess(trainingProgramsList.loadingStatus) && trainingProgramsList.data.length == 0) && (
-          <UIAlert severity='warning' title='У вас нет программ тренировок на текущий момент' />
+          <UIAlert severity='warning' title='У вас нет активных программ тренировок на текущий момент' />
         )}
 
         {(isSuccess(trainingProgramsList.loadingStatus) && trainingProgramsList.data.length > 0) && (  
@@ -48,7 +48,7 @@ function TrainingPrograms() {
               trainingProgramsList?.data?.map(program => (
               <CustomCard 
                 sx={{  
-                  p: 1.5,
+                  p: 2,
                   width: "100%",
                   justifyContent: 'center',
                   gap: 0,
@@ -56,21 +56,60 @@ function TrainingPrograms() {
                   "&:hover": { boxShadow: 0 }
                 }}
               >
-                <Grid2 container spacing={1.5}>
+                <Grid2 container spacing={1.25}>
                   <Grid2 size={12} container spacing={0.5} sx={{ flexDirection: 'column' }}>
                     <Typography sx={{ fontSize: 16, fontWeight: 600 }} color={theme.palette.grey[900]}>{program.name}</Typography>
-                    <Typography sx={{ pb: 0.5 }} variant="body2" color={theme.palette.grey[500]}>{program.description}</Typography>
-                    <Divider  flexItem/>
+                    <Typography sx={{ pb: 1 }} variant="body2" color={theme.palette.grey[500]}>{program.description}</Typography>
+                    <Divider flexItem/>
                   </Grid2>
 
-                  <Grid2 container spacing={0.5} sx={{ flexGrow: 1 }}>
-                    {program.exercises.map(exercise => (
-                      <Grid2 size={12} container spacing={0.5} sx={{  }}>
-                        <Typography>{exercise.name}</Typography>
+                  <Grid2 container>
+                    {program.exercises.map((exercise, index) => (
+                      <Grid2 
+                        key={exercise.exercise_id} 
+                        container 
+                        size={12} 
+                        sx={{ 
+                          alignItems: 'center', 
+                          p: 1.5,
+                          borderRadius: 1,
+                          transition: '450ms ease',
+                          '&:hover': { bgcolor: theme.palette.gray[50] } 
+                        }}
+                      >
+                        <Grid2 size="auto" sx={{ pr: 0.5 }}>
+                          <Typography variant="caption" color={theme.palette.gray[500]}>{++index}</Typography>
+                        </Grid2>
+
+                        <Grid2 size sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', gap: 0.5 }}>
+                          <Grid2 sx={{ display: 'flex' }} gap={0.75}>
+                            <Typography sx={{ fontWeight: 500, color: theme.palette.gray[900] }}>{exercise.name}</Typography>
+                              <Typography sx={{ fontSize: 12, fontWeight: 300, color: theme.palette.gray[500] }}>
+                              {exercise.reps}х{exercise.sets}
+                            </Typography>
+                          </Grid2>
+                          <Grid2 sx={{ display: 'flex' }} gap={0.4}>
+                            <Typography sx={{ fontWeight: 300, fontSize: 12, color: theme.palette.gray[500] }}>
+                              {exercise.recent_weight}{exercise.unit}
+                            </Typography>
+                            <Typography sx={{ fontWeight: 300, fontSize: 12, color: theme.palette.gray[500] }}>
+                              {exercise.recent_reps}х{exercise.recent_sets}
+                            </Typography>
+                            <Typography sx={{ fontWeight: 300, fontSize: 12, color: theme.palette.gray[500] }}>
+                              {exercise.recent_created_on_tz 
+                                ?  `от ${dayjs(exercise.recent_created_on_tz).format('D MMM YYYY')}`
+                                : null
+                              }                           
+                            </Typography>
+                          </Grid2>
+                        </Grid2>
+
+                        <Grid2 size="auto">
+                          <Chip label={exercise.muscle_group}/>
+                        </Grid2>
                       </Grid2>
                     ))}
                   </Grid2>
-
                 </Grid2>
               </CustomCard>
             ))   
